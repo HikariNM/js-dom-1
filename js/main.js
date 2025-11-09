035eee2b35bf40c45e73100fb78783a5073b7851
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-09 18:34:25 +0100

    aggiunta altra funzione al passaggio del mouse

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 function switchOnAndOff() {
     const bulb = document.getElementById('ligth');
     const btn = document.getElementById('switchBtn')
-    
+
     if (bulb.src.includes('white_lamp')) {
         bulb.src = './img/yellow_lamp.png';
         btn.textContent = 'Spegni';
-        bulb.classList.replace('ps-5','pe-5')
+        bulb.classList.replace('ps-5', 'pe-5')
     }
     else {
         bulb.src = './img/white_lamp.png';
         btn.textContent = 'Accendi';
-        bulb.classList.replace('pe-5','ps-5')
+        bulb.classList.replace('pe-5', 'ps-5')
     }
-    
+
 }
 
 document.getElementById('switchBtn').addEventListener('click', switchOnAndOff)
+
+document.getElementById('ico').addEventListener('mouseover', () => {
+    document.getElementById('ico').src = './img/yellow_lamp.png';
+});
+
+document.getElementById('ico').addEventListener('mouseout', () => {
+    document.getElementById('ico').src = './img/white_lamp.png';
+});
+
